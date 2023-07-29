6f8be1731bd741e3f27d11868eb438fab9bcb646
Jasleen8801 <jkaur1_be21@thapar.edu>
2023-07-29 13:48:50 +0530

    noice

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from .models import Post
 
@@ -17,16 +19,37 @@ from .models import Post
 #     }
 # ]
 
+
 def home(request):
     # return HttpResponse('<h1>Blog Home</h1>')
 
     context = {
         # 'posts': posts
-        'posts': Post.objects.all()
+        "posts": Post.objects.all()
     }
-    return render(request, 'blog/home.html', context)
+    return render(request, "blog/home.html", context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html"  # <app>/<model>_<viewtype>.html
+    context_object_name = "posts"
+    ordering = ["-date_posted"]
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
     # return HttpResponse('<h1>Blog About</h1>')
-    return render(request, 'blog/about.html', {'title': 'About Page'})
+    return render(request, "blog/about.html", {"title": "About Page"})
